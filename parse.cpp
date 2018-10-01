8f88fc26717d32dfa04372d06551ef0ac5900a7a
Ashutosh Bindal <ashutoshbindal@gmail.com>
2018-10-01 23:42:05 +0530

    output optimize

--- parse.cpp
+++ parse.cpp
@@ -47,13 +47,13 @@ int main(int argc, char* argv[]){
     fin.close();
 
     //literal
-    int m[n][k];
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<k; j++){
-            m[i][j] = 0;
-        }
-    }
+    // int m[n][k];
+    //
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<k; j++){
+    //         m[i][j] = 0;
+    //     }
+    // }
 
     // //reading form the output file
     string str;
@@ -70,34 +70,50 @@ int main(int argc, char* argv[]){
     input_file = input_file + ".satoutput";
 
     fin.open(input_file);
-    getline(fin, str);
-
+    // getline(fin, str);
+    fin>>str;
     //debugging
     // cout<<str<<endl;
-    
+
     if(str == "SAT"){
-        while(getline(fin, str)){
-            //process the satisfiability
-            vector<string> temp;
-            split(str, " ", temp);
-            temp.pop_back();
 
-            //translating the output into matrix
-            int iter = 0;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<k; j++){
-                    string temp_literal = temp[iter];
-                    int lit = stoi(temp_literal);
-                    if(lit > 0){
-                        m[i][j] = 1;
-                    }
+        //new method
+        int number;
+        vector<int> ans_vec[k];
 
-                    iter++;
+        for(int j=0; j<n; j++){
+            for(int i=0; i<k; i++){
+                fin>>number;
+                if(number>0){
+                    // cout<<number<<endl;
+                    // number = number - (j*k);
+                    ans_vec[i].push_back(j+1);
                 }
             }
-            //conditional
-            break;
         }
+
+        // while(getline(fin, str)){
+        //     //process the satisfiability
+        //     vector<string> temp;
+        //     split(str, " ", temp);
+        //     temp.pop_back();
+        //
+        //     //translating the output into matrix
+        //     int iter = 0;
+        //     for(int i=0; i<n; i++){
+        //         for(int j=0; j<k; j++){
+        //             string temp_literal = temp[iter];
+        //             int lit = stoi(temp_literal);
+        //             if(lit > 0){
+        //                 m[i][j] = 1;
+        //             }
+        //
+        //             iter++;
+        //         }
+        //     }
+        //     //conditional
+        //     break;
+        // }
 
         //writing in the output file
         //generating the file name
@@ -112,24 +128,41 @@ int main(int argc, char* argv[]){
         output_file = output_file + ".subgraphs";
 
         fout.open(output_file, std::ios_base::app);
-        for(int i=1; i<=k; i++){
-            int node_exist = 0;
-            for(int j=0; j<n; j++){
-                if(m[j][i-1] == 1){
-                    node_exist++;
+        // for(int i=1; i<=k; i++){
+        //     int node_exist = 0;
+        //     for(int j=0; j<n; j++){
+        //         if(m[j][i-1] == 1){
+        //             node_exist++;
+        //         }
+        //     }
+        //     fout<<"#"<<to_string(i)<<" "<<to_string(node_exist)<<"\n";
+        //     bool flag = false;
+        //     for(int j=0; j<n; j++){
+        //         if(m[j][i-1] == 1){
+        //             if(!flag){
+        //                 fout<<to_string(j+1);
+        //                 flag = true;
+        //             }
+        //             else{
+        //                 fout<<" "<<to_string(j+1);
+        //             }
+        //         }
+        //     }
+        //     fout<<"\n";
+        // }
+
+        //new method
+        for(int i=1; i<=k;i++){
+            vector<int> temp_ans = ans_vec[i-1];
+            fout<<"#"<<to_string(i)<<" "<<to_string(temp_ans.size())<<"\n";
+            bool flag = true;
+            for(int j=0; j<temp_ans.size(); j++){
+                if(flag){
+                    fout<<to_string(temp_ans[j]);
+                    flag = false;
                 }
-            }
-            fout<<"#"<<to_string(i)<<" "<<to_string(node_exist)<<"\n";
-            bool flag = false;
-            for(int j=0; j<n; j++){
-                if(m[j][i-1] == 1){
-                    if(!flag){
-                        fout<<to_string(j+1);
-                        flag = true;
-                    }
-                    else{
-                        fout<<" "<<to_string(j+1);
-                    }
+                else{
+                    fout<<" "<<to_string(temp_ans[j]);
                 }
             }
             fout<<"\n";
